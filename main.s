fbe92042c7f4623eab0fac9a2f5ccfc267e24047
ojphi <ojphi@DESKTOP-J3NHJM4>
2023-12-01 10:30:24 +0000

    Update of some of the ultrasonic rangefinder code. Still working on it but hope to be done with it today.

--- main.s
+++ main.s
@@ -1,6 +1,6 @@
 #include <xc.inc>
 
-psect	code, abs
+psect usrf ; Defining a psect
 
 cblock 0x20 ; Define variables used
 delay1
@@ -8,31 +8,46 @@ delay2
 delay3
 endc
 
-init: 
-    org 0x00 ; Reset vector
-    goto main
+usrf_main: ; This would be shortened to rf but I want to avoid any
+	   ; confusion with radio frequency  
+    usrf_init:
+	call trig_one ; Activate the first rangefinder
+	call delay_long ; Wait for the pulse to finish.
+	call trig_two ; Activate the second rangefinder.
+	call convert ; Convert the time delay to metres. 
+
+    trig_one: ; Create a 10 us pulse at the RB3 pin which is connected to the first
+	      ; ultrasonic sensor 	
+      movlw 0x00
+      movwf TRISH ; Set all pins as outputs.
+      bsf LATH, 3 ; Set pin 3 on PORTH high.
+      call delay_short
+      bsf LATH, 3 ; Set pin 3 on PORTH low.
+      movlw 0xFF
+      movwf TRISH ; Set all pins as inputs
+      
+      
+;     movlw TRISH, 3 ; Set all pins as output. 
+;     movlw 0xBF
+;     movf LATH
+;     call delay_short ; Call the 10 us delay subroutine.
+;     bsf TRISH, 3 ; Set pin 3 as input.
+;     return 
 
 loop:
     call trig_one
     call delay_long
-    goto loop
+    goto loop 
     
-trig_one: ; Create a 10 us pulse at the RB3 pin which is connected to the first
-	  ; ultrasonic sensor 
-    bcf TRISB, 3 ; Set pin 3 as output. 
-    movlw 0xBF
-    movf LATB
-    call delay_short ; Call the 10 us delay subroutine.
-    bsf TRISB, 3 ; Set pin 3 as input.
-    return
+
      
 trig_two: ; Create a 10 us pulse at the RB3 pin which is connected to the second
 	  ; ultrasonic sensor
-    bcf TRISB, 4 ; Set pin 4 as output. 
+    bcf TRISH, 4 ; Set pin 4 as output. 
     movlw 0xFA 
-    movf LATB ; Set output
+    movf LATH ; Set output
     call delay_short ; Call the 10 us delay subroutine.
-    bsf TRISB, 4 ; Set pin 4 as input.
+    bsf TRISH, 4 ; Set pin 4 as input.
     return
     
 delay_short: ; Generate a 10 us delay. Clock frequency is 16 MHz so each
@@ -51,17 +66,22 @@ delay_long: ; Generate a 60 ms delay. Made using an online delay generator.
     movlw 0x02 ; 2
     movwf delay3
     call delay_loop
-
-delay_loop:
-    decfsz delay1, 1
-    goto delay_loop
-    decfsz delay2, 1
-    goto delay_loop
-    decfsz delay3, 1
-    goto delay_loop
-    nop
-    nop
-    return    
+    delay_loop:
+	decfsz delay1, 1
+	goto delay_loop
+	decfsz delay2, 1
+	goto delay_loop
+	decfsz delay3, 1
+	goto delay_loop
+	nop
+	nop
+	return
     
+	
 start_timer:
-    
+    movlw 0b10000111
+    movwf T1CON
+    bsf TMR1IE
+    bsf GIE
+    movlw 
+    movwf 0xFF
