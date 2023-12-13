e9965df6cbb0adc94efb83e11eb97330c0abbd2a
Oliver Phillips <oliver.phillips20@imperial.ac.uk>
2023-12-13 14:47:22 +0000

    debugging itnerrupts

--- hex_to_dec.s
+++ hex_to_dec.s
@@ -1,250 +1,121 @@
-#include <xc.inc>
-     
-extrn time_H, time_L
- 
-global dist_H, dist_L
-
-psect	udata_acs  
-
-; ds reserve space
-; equ placeholder
-; set ??	
-	
-d_pulse:	ds 1    
-result_LL:	ds 1	; The low byte of the product using the low byte
-result_LH:	ds 1	; etc
-result_HL:	ds 1	; etc
-result_HH:	ds 1	; The high byte of the product using the high byte    
-dist_L:		ds 1	; Low byte of the conversion
-dist_H:		ds 1	; High byte of the conversion
- 
-psect hex_code, class = CODE
-
-org 0x00
-start:
-    goto time_to_dist
-    goto dist_to_ascii
-    
-time_to_dist:
-    ; 8 bit by 16 bit multiplication
-    movlw   0x56		
-    movf    d_pulse		; Tick distance ~ 86 um
-    movlw   time_L
-    mulwf   d_pulse		; d_pulse * time_h -> PRODH:PRODL
-    movff   PRODL, result_LL
-    movff   PRODH, result_LH
-    movlw   time_H
-    mulwf   d_pulse		; d_pulse * time_h -> PRODH:PRODL
-    movff   PRODL, result_HL
-    movff   PRODH, result_HH
-    return
-
-dist_to_ascii:
-    ; Convert a 32 bit number into a string of ASCII denary digits for output
-    ; to the LCD
-    
-    
-    
+;#include <xc.inc>
+;     
+;extrn time_H, time_L
+; 
+;global dist_H, dist_L
+;
+;psect	udata_acs  	
+;	
+;d_pulse:	ds 1    
 ;    
-;    ;in: BIN
-;;out huns. tens, ones
-;
-;;uses ADD-3 algoerthm
-;
-;movlw 8
-;movwf count
-;clrf huns
-;clrf tens
-;clrf ones
-;
-;BCDADD3
-;
-;movlw 5
-;subwf huns, 0
-;btfsc STATUS, C
-;CALL ADD3HUNS
-;
-;movlw 5
-;subwf tens, 0
-;btfsc STATUS, C
-;CALL ADD3TENS
-;
-;movlw 5
-;subwf ones, 0
-;btfsc STATUS, C
-;CALL ADD3ONES
-;
-;decf count, 1
-;bcf STATUS, C
-;rlf BIN, 1
-;rlf ones, 1
-;btfsc ones,4 ; 
-;CALL CARRYONES
-;rlf tens, 1
-;
-;btfsc tens,4 ; 
-;CALL CARRYTENS
-;rlf huns,1
-;bcf STATUS, C
-;
-;movf count, 0
-;btfss STATUS, Z
-;GOTO BCDADD3
-;
-;
-;movf huns, 0 ; add ASCII Offset
-;addlw h'30'
-;movwf huns
-;
-;movf tens, 0 ; add ASCII Offset
-;addlw h'30'
-;movwf tens
-;
-;movf ones, 0 ; add ASCII Offset
-;addlw h'30'
-;movwf ones
-;
-;RETURN
-;
-;ADD3HUNS
-;
-;movlw 3
-;addwf huns,1
-;
-;RETURN
-;
-;ADD3TENS
-;
-;movlw 3
-;addwf tens,1
-;
-;RETURN
-;
-;ADD3ONES
-;
-;movlw 3
-;addwf ones,1
-;
-;RETURN
-;
-;CARRYONES
-;bcf ones, 4
-;bsf STATUS, C
-;RETURN
-;
-;CARRYTENS
-;bcf tens, 4
-;bsf STATUS, C
-;RETURN
+;result_LL:	ds 1	; The low byte of the product 
+;result_MM:	ds 1	; The middle byte of the product
+;result_LH:	ds 1	; Intermediate byte
+;result_HL:	ds 1	; Intermediate byte
+;result_HH:	ds 1	; The high byte of the product 
 ;    
-
-;bin2dec	
-;	clrf	MTEMP		;Reset sign flag
-;	call	absa		;Make REGA positive
-;	skpnc
-;	return			;Overflow
-;
-;	call	clrdig		;Clear all digits
-;
-;	movlw	D'32'		;Loop counter
-;	movwf	MCOUNT
-;
-;b2dloop	
-;	rlf	REGA0,f		;Shift msb into carry
-;	rlf	REGA1,f
-;	rlf	REGA2,f
-;	rlf	REGA3,f
-;
-;	movlw	DIGIT10
-;	movwf	FSR		;Pointer to digits
-;	movlw	D'10'		;10 digits to do
-;	movwf	DCOUNT
-;
-;adjlp	rlf	INDF,f		;Shift digit and carry 1 bit left
-;	movlw	D'10'
-;	subwf	INDF,w		;Check and adjust for decimal overflow
-;	skpnc
-;	movwf	INDF
-;
-;	decf	FSR,f		;Next digit
-;	decfsz	DCOUNT,f
-;	goto	adjlp
-;
-;	decfsz	MCOUNT,f	;Next bit
-;	goto	b2dloop
-;
-;	btfsc	MTEMP,0		;Check sign
-;	bsf	DSIGN,0		;Negative
-;	clrc
-;	return
+;dist_L:		ds 1	; Low byte of the conversion
+;dist_H:		ds 1	; High byte of the conversion
 ;    
-;Hundreds	equ 0x20
-;Tens		equ 0x21
-;units		equ 0x22
-;working		equ 0x23
-;numtocon	equ 0x24
-;count		equ 0x25
-;   	
-;	ORG 0x0000 ;reset vector 
+;dgt_0:		ds 1	; Units of um
+;dgt_1:		ds 1	; Tens of um
+;dgt_2:		ds 1	; Hundreds of um
+;dgt_3:		ds 1	; Units of mm
+;dgt_4:		ds 1	; Tens of mm
+;dgt_5:		ds 1	; Hundreds of mm
+;dgt_6:		ds 1	; Units of m
+;dgt_7:		ds 1	; Tens of m
 ;
-;	goto start
+;psect hex_code, class = CODE
 ;
-;
-;Start
-;		clrf	Hundreds
-;		clrf	Tens
-;		clrf	Units
-;
-;		movlw	d'159'	  ;load w with the number to covert
-;		movwf	numtocon  ;save the number in variable	
-;
-;		movlw	d'8'
-;		movwf	count		
-;ShiftLoop
-;;test lowerbcd		
-;		movfw	units
-;		andlw	b'1111'
-;		movwf	working
-;		movlw	d'5'
-;		subwf	working,0
-;		SKPC		
-;		Goto	TestUpperNibble
-;		movlw	d'3'
-;		addwf	units
-;TestUpperNibble		
-;		movfw	units
-;		andlw	b'11110000'
-;		movwf	working
-;		movlw	H'50'
-;		subwf	working,0
-;		SKPC		
-;		goto	Shiftbit
-;		movlw	H'30'
-;		addwf	units
-;shiftbit
-;		rlf	units,1
-;		rlf	hundreds,1
-;		SKPNC
-;		bsf	hundreds,0
-;		rlf	numtocon,1
-;		SKPNC	
-;		bsf	Units,0
-;		decfsz	count
-;		goto	ShiftLoop
-;		movfw	Units
-;		Movwf	Tens
-;		swapf	Tens,1
-;		movlw	b'1111'
-;		andwf	Units,1
-;		andwf	Tens,1
-;		movlw	d'48'
-;		addwf	Hundreds
-;		addwf	Tens
-;		addwf	Units
+;org 0x00
+;start:
+;    goto time_to_dist
+;    goto dist_to_ascii
+;    
+;time_to_dist:
+;    ; 8 bit by 16 bit multiplication results in a 24 bit number
+;    clrf    STATUS
+;    movlw   0x56		
+;    movwf   d_pulse		; Tick distance ~ 86 um
+;    movf    time_L
+;    mulwf   d_pulse		; d_pulse * time_h -> PRODH:PRODL
+;    movff   PRODL, result_LL
+;    movff   PRODH, result_LH
+;    movf    time_H
+;    mulwf   d_pulse		; d_pulse * time_h -> PRODH:PRODL
+;    movff   PRODL, result_HL
+;    movff   PRODH, result_HH
+;    movf    result_LH
+;    addwf   result_MM		; Add first medium product byte
+;    movf    result_HL
+;    addwf   result_MM		; Add second medium product byte
+;    btfss   STATUS, 0
+;    incf    result_HH
+;    return
 ;
 ;
-;;your code here to display variables on LCD
-
-
-
-
+;bin24dec:
+;    call    clr_dgts		; Clear the 
+;    movlw   0x18		;24 bits to do
+;    movwf   bit_count
+;    
+;bitlp:
+;    rlcf    result_LL		; Shift most significant bit into carry
+;    rlcf    result_MM
+;    rlcf    result_HH
+;    lfsr    FSR0, dgt_0		; load FSR0 with dgt_0
+;    movlw   0x08		; 8 digits to do
+;    movwf   dgt_cnt
+;
+;adjlp:
+;    rlcf    INDF0	;Shift digit 1 bit left
+;    movlw   0x0A
+;    subwf   INDF0,w,0	;Check and adjust for decimal overflow
+;    btfsc   STATUS,0
+;;skpnc
+;    movwf   INDF0
+;    movf    POSTINC0,w
+;    decfsz  digcnt
+;    goto    adjlp
+;    decfsz  bitcnt ;Next bit
+;    goto    bitlp
+;    return
+;
+;clr_dgts:
+;    clrf    dgt_0
+;    clrf    dgt_1
+;    clrf    dgt_2
+;    clrf    dgt_3
+;    clrf    dgt_4
+;    clrf    dgt_5
+;    clrf    dgt_6
+;    clrf    dgt_7
+;return 
+;
+;;dist_BCD:
+;;    ; Convert a 32 bit number into a string of ASCII denary digits for output
+;;    ; to the LCD made using a modified version of the double dabble algorithm
+;;    ; which converts binary into binary coded digits which are then converted
+;;    ; to ASCII.
+;;    clrf    dgt_0
+;;    clrf    dgt_1
+;;    clrf    dgt_2
+;;    clrf    dgt_3
+;;    clrf    dgt_4
+;;    clrf    dgt_5
+;;    clrf    dgt_6
+;;    clrf    dgt_7
+;;    movlw   0x18
+;;    movf    counter_1
+;;    goto    shift_1
+;;    
+;;ADJBCD:  	
+;;    MOVLW   DIGIT1
+;;    MOVWF   FSR
+;;    MOVLW   7
+;;    MOVWF   COUNTER2
+;;    MOVLW   3    
+;;    
+;;shift_1: 
+;;    call
--- us_itp_test.s
+++ us_itp_test.s
@@ -1,144 +1,384 @@
 #include <xc.inc>
-			  
+    
 extrn pulse_delay, wait_delay
-        
-flag_reg set 0x20	    ; Define a flag to reflect if the first range finder
-			    ; has been activated or not.
+
+flag	set 0x20
+    
 psect udata_acs
-time_one_H:	ds 1
-time_one_L:	ds 1
-time_two_H:	ds 1
-time_two_L:	ds 1
-			    
-global ranger_main, time_one_H, time_one_L, time_two_H, time_two_L 
 
+time_H:		ds 1
+time_L:		ds 1
+
+global	ranger_main, time_H, time_L
+    
 psect ranger_code, class = CODE, abs
-
+    
 org 0x00
 start:
     goto    ranger_main
-
-org 0x08
+    
 interrupt_handler:
-    ; Interrupt vector
-    btfss   flag_reg, 0	    ; Check if the first range finder has been used
-    goto    interrupt_one   ; NO, measure the first range finder
-    goto    interrupt_two   ; YES, measure the second range finder
-    return  
-
-org 0x100
+    org 0x08
+    btfss   flag, 0
+    goto    rising_edge
+    goto    falling_edge
+    
 ranger_main:
     call    ranger_init
-    call    trigger_one	    ; Trigger the first range finder
-    bsf	    flag_reg,	0   ; Set the flag_reg bit to show that the first range
-			    ; finder has been activated
-    call    trigger_two
+    call    trigger
+    call    wait_delay
     goto    ranger_main
-    return
     
 ranger_init:
-    clrf    time_one_H
-    clrf    time_one_L
-    clrf    time_two_H
-    clrf    time_two_L
+    bsf	    INTCON, 6	; Enable peripheral interrupt
     bsf	    INTCON, 7	; Enable global interrupt
-    bsf	    INTCON, 6   ; Enable peripheral interrupt
-    bsf	    PIE4,   1	; Enable CCP4 interrupt
-    bsf	    PIE4,   2	; Enable CCP5 interrupt
-    bsf	    IPR4,   1	; Set CCP4 interrupt as high priority
-    bsf	    IPR4,   2	; Set CCP5 interrupt as high priority
-    clrf    flag_reg	; Clear flag register
-    return 
-        
-interrupt_one:
-    bcf	    PIE4,	1   ; Pause any other interrupts from CCP4
-    bcf	    PIR4,	1   ; CCP4IF, clear the interrupt flag
-    btfss   flag_reg,	1   ; Check if interrupt has already occurred
-    goto    rising_one	    ; NO, start timer on the rising edge
-    goto    falling_one	    ; YES, stop timer and move values into memory
-        
-interrupt_two:
-    bcf	    PIE4,	2   ; Pause any other interrupts from CCP5
-    bcf	    PIR4,	2   ; CCP5IF, clear the interrupt flag 	
-    btfss   flag_reg,	2   ; Check if interrupt has already occurred
-    goto    rising_two	    ; NO, start the timer on the rising edge
-    goto    falling_two	    ; YES, stop timer and move values into memory
-   
-rising_one:
-    clrf    CCP4CON
-    movlw   0x04    
-    movwf   CCP4CON	; Set the CCP4 control byte to capture mode and to 
-			; trigger on the falling edge now
-    clrf    CCPTMRS1 
-    movlw   0x00	; CCP4 is based off of TMR1
-    movwf   CCPTMRS1		
-    bsf	    PIE4,   1	; Re-enable interrupts from CCP4
-    movlw   0x31	; Set the TMR1 to use F_osc/4 prescaled by 8
-    movwf   T1CON	; Start the timer
+    bsf	    PIE4, 1	; Set CCP4 interrupt
+    bsf     IPR4, 1     ; Set CCP4 interrupt as high priority
+    clrf    PIR4	; Clear interrupt flag register
+    clrf    flag	; Clear sequence flag register
     return
     
-falling_one:
-    bcf	    PIE4,   1		; Disable CCP4 interrupts to prevent any further
-				; interrupts
-    bsf	    T1CON, 0		; Turn TMR1 off
-    movff   TMR1L, time_one_L	; Move high byte into memory
-    movff   TMR1H, time_one_H	; Move low byte into memory
-    goto    ranger_main
-    
-    
-rising_two:
-    bcf	    PIE4,   1	; Disable CCP4 interrupts to prevent any further
-			; interrupts
-    clrf    CCP5CON
-    movlw   0x04    
-    movwf   CCP5CON	; Set the CCP5 control byte to capture mode and to 
-			; trigger on the falling edge now
-    clrf    CCPTMRS1 
-    movlw   0x00	; CCP5 is based off of TMR1
-    movwf   CCPTMRS1	
-    movlw   0x31	; Set the TMR1 to use F_osc/4 prescaled by 8
-    movwf   T1CON
-    return
-    
-falling_two:
-    bcf	    PIE4,   2		; Disable CCP4 interrupts to prevent any further
-				; interrupts
-    bsf	    T1CON,  0		; Turn TMR1 off
-    movff   TMR1L, time_two_L	; Move high byte into memory
-    movff   TMR1H, time_two_H	; Move low byte into memory
-    return
-    
-trigger_one:
-    ; Initialize the CCP interrupt and trigger the ultrasonic range finder.
-    bcf	    PIR4,   1	; CCP4IF, clear the interrupt flag 
-    clrf    CCP4CON
+trigger:
+    ; Initialize the CC64 interrupt and trigger the ultrasonic range finder.
     movlw   0x00
     movwf   TRISG	; Make all pins on PORTG outputs.
-    bsf	    LATG,   3	; Set pin 3 to be high.
+    bsf     LATG, 3	; Set pin 3 to be high.
     call    pulse_delay 
-    bcf	    LATG,   3	; Set pin 3 to be low.
+    bcf     LATG, 3	; Set pin 3 to be low.
     movlw   0xFF
     movwf   TRISG	; Make all pins on PORTG inputs
-    movlw   0x05	; Interrupt on rising edge.
+    clrf    CCP4CON    
+    movlw   0x05	; Set the CCP control byte to capture mode and to
+			; trigger on the rising edge (CCP4M = 0101)
     movwf   CCP4CON
     call    wait_delay
     return
-
-trigger_two:
-    ; Initialize the CCP4 capture module interrupt and trigger the ultrasonic 
-    ; range finder.
-    bcf	    PIR4,   2	; CCP5IF, clear the interrupt flag 	
-    clrf    CCP5CON
+ 
+rising_edge:
+    bcf	    PIE4, 1	; Temporarily disable interrupts on CCP4
+    clrf    CCP4CON
+    movlw   0x04
+    movwf   CCP4CON	; Set the CCP4 control byte to capture mode and to 
+			; trigger on the falling edge (CCP4M = 0100)
+    clrf    CCPTMRS1
     movlw   0x00
-    movwf   TRISG	; Make all pins on PORTH outputs.
-    bsf	    LATG,   4	; Set pin 4 to be high.
-    call    pulse_delay 
-    bcf	    LATG,   4	; Set pin 4 to be low.
-    movlw   0xFF
-    movwf   TRISG	; Make all pins on PORTH inputs.
-    movlw   0x05	; Interrupt on rising edge.
-    movwf   CCP5CON
-    call    wait_delay
+    movwf   CCPTMRS1	; CCP4 is based on TMR1	
+    clrf    T1CON
+    movlw   0x31
+    movwf   T1CON	; Set TMR1 to use F_osc/4 prescaled by a factor of 8
+    movwf   TRISD	; Make all pins on PORTD outputs.
+    bsf     LATD, 3	; Set pin 3 to be high.
+    call    wait_delay	; Illuminate to verify interrupt operation
+    bcf     LATD, 3	; Set pin 3 to be low.
+    bsf	    PIE4, 1	; Re-enable interrupts on CCP5
+    
+falling_edge:
+    bcf	    PIE4, 1	    ; Temporarily disable interrupts on CCP6
+    bsf	    T1CON, 0	    ; Turn off TMR1
+    bsf	    PIE4, 1	    ; Re-enable interrupts on CCP6
+    movff   TMR1H, time_H
+    movff   TMR1L, time_L
+    movlw   0x00
+    bsf	    PIE4, 1	; Re-enable interrupts on CCP4
     return
+    
+;    
+;falling_one:
+;    bcf	    PIE4,   1	; Temporarily disable interrupts
+;    bsf     T1CON, 0         ; Turn TMR1 off
+;    movff   TMR1L, time_one_L
+;    movff   TMR1H, time_one_H
+;    
+;    goto    ranger_main
+;    
+
+
+;#include <xc.inc>
+;    
+;
+;    
+;extrn pulse_delay, wait_delay
+;        
+;trigger_flag set 0x20           ; Define a flag to reflect if the first range finder
+;                                ; has been activated or not.
+;psect udata_acs
+;time_one_H:       ds 1
+;time_one_L:       ds 1
+;time_two_H:      ds 1
+;time_two_L:       ds 1
+;                                                 
+;global ranger_main, time_one_H, time_one_L, time_two_H, time_two_L 
+;
+;psect ranger_code, class = CODE, abs
+;
+;org 0x00
+;start:
+;    goto    ranger_main
+;
+;org 0x08
+;interrupt_handler:
+;    ; Interrupt vector
+;    btfss   trigger_flag, 0 ; Check if the first range finder has been used.
+;    goto    interrupt_one   ; NO, measure the first range finder.
+;    goto    interrupt_two   ; YES, measure the second range finder.
+;    return  
+;
+;org 0x100
+;ranger_main:
+;    call    ranger_init
+;    clrf    trigger_flag
+;    call    trigger_one
+;    bsf     trigger_flag, 0
+;    call    trigger_two
+;    goto    ranger_main
+;    
+;ranger_init:
+;    clrf	 PIR4
+;    bsf          PIE4,   1           ; Enable CCP4 interrupt
+;    bsf          PIE4,   2           ; Enable CCP5 interrupt
+;    bsf          INTCON, 6	     ; Enable peripheral interrupt
+;    bsf          INTCON, 7	     ; Enable global interrupt
+;    bsf          IPR4,   1           ; Set CCP4 interrupt as high priority
+;    bsf          IPR4,   2           ; Set CCP5 interrupt as high priority
+;    return 
+;    
+;    
+;interrupt_one:
+;    btfss   PIR4, 1		; Check if interrupt has already occurred. 
+;    goto    rising_one          ; NO, start the timer on the rising edge.
+;    goto    falling_one         ; YES, stop the timer and move the values into memory.
+;        
+;    
+;rising_one:
+;    bcf	    PIE4,   1	; Temporarily disable interrupts
+;    clrf    CCP4CON
+;    movlw   0x04    
+;    movwf   CCP4CON       ; Set the CCP4 control byte to capture mode and to 
+;			  ; trigger on the falling edge now.
+;    clrf    CCPTMRS1 
+;    movlw   0x00  ; CCP4 is based off of TMR1.
+;    movwf   CCPTMRS1     
+;    movlw   0x31  ; Set the TMR1 to use F_osc/4 prescaled by 8.
+;    movwf   T1CON
+;    bsf	    PIE4, 1	; Re-enable
+;    retfie
+;    
+;falling_one:
+;    bcf	    PIE4,   1	; Temporarily disable interrupts
+;    bsf     T1CON, 0         ; Turn TMR1 off
+;    movff   TMR1L, time_one_L
+;    movff   TMR1H, time_one_H
+;    
+;    goto    ranger_main
+;    
+;interrupt_two:
+;    btfss   PIR4, 2 ; Check if interrupt has already occurred. 
+;    goto    rising_two          ; NO, start the timer on the rising edge.
+;    goto    falling_two        ; YES, stop the timer and move the values into memory.
+;    
+;rising_two:
+;    clrf    CCP5CON
+;    movlw   0x04    
+;    movwf   CCP5CON       ; Set the CCP5 control byte to capture mode and to 
+;                                             ; trigger on the falling edge now.
+;    clrf    CCPTMRS1 
+;    movlw   0x04  ; CCP5 is based off of TMR1.
+;    movwf   CCPTMRS1     
+;    movlw   0x31  ; Set the TMR1 to use F_osc/4 prescaled by 8.
+;    movwf   T1CON
+;    retfie
+;    
+;falling_two:
+;    bcf	    PIE4,   2	; Temporarily disable interrupts
+;    bsf     T1CON, 0         ; Turn TMR1 off
+;    movff   TMR1L, time_two_L
+;    movff   TMR1H, time_two_H
+;    
+;    return
+;
+;; exponential overshoot decreases the rising time
+;    
+;; this might be benefical
+;    
+;; increases inductance and capacitance
+;    
+;trigger_one:
+;    ; Initialize the CCP interrupt and trigger the ultrasonic range finder.
+;    bcf     PIR4, 1             ; CCP4IF, clear the interrupt flag 
+;    clrf    CCP4CON
+;    movlw   0x00
+;    movwf   TRISG              ; Make all pins on PORTH outputs.
+;    bsf     LATG, 3            ; Set pin 3 to be high.
+;    call    pulse_delay 
+;    bcf     LATG, 3            ; Set pin 3 to be low.
+;    movlw   0xFF
+;    movwf   TRISG              ; Make all pins on PORTH inputs
+;    movlw   0x05  ; Interrupt on rising edge.
+;    movwf   CCP4CON
+;    call    wait_delay
+;    return
+;
+;trigger_two:
+;    ; Initialize the CCP4 capture module interrupt and trigger the ultrasonic 
+;    ; range finder.
+;    bcf     PIR5, 1             ; CCP5IF, clear the interrupt flag 
+;    clrf    CCP5CON
+;    movlw   0x00
+;    movwf   TRISG		; Make all pins on PORTH outputs.
+;    bsf     LATG, 4		; Set pin 4 to be high.
+;    call    pulse_delay 
+;    bcf     LATG, 4		; Set pin 4 to be low.
+;    movlw   0xFF
+;    movwf   TRISG		; Make all pins on PORTH inputs.
+;    movlw   0x05		; Interrupt on rising edge.
+;    movwf   CCP5CON
+;    call    wait_delay
+;    return
+
+;#include <xc.inc>
+;			  
+;extrn pulse_delay, wait_delay
+;    
+;;flag_reg set 0x20	    ; Define a flag to reflect if the first range finder
+;;			    ; has been activated or not.    
+;
+;psect udata_acs
+;flag_reg:	ds 1	    ; Define a flag to reflect if the first range finder
+;			    ; has been activated or not.
+;time_one_H:	ds 1
+;time_one_L:	ds 1
+;time_two_H:	ds 1
+;time_two_L:	ds 1
+;			    
+;global ranger_main, time_one_H, time_one_L, time_two_H, time_two_L 
+;
+;psect ranger_code, class = CODE, abs
+;
+;org 0x00
+;start:
+;    goto   ranger_main
+;
+;org 0x08
+;interrupt_handler:
+;    ; Interrupt vector
+;;    btfss   flag_reg, 0	    ; Check if the first range finder has been used
+;;    goto    interrupt_one   ; NO, measure the first range finder
+;    goto    interrupt_two   ; YES, measure the second range finder
+;    return  
+;
+;org 0x100
+;ranger_main:
+;    call    ranger_init
+;;    call    trigger_one	    ; Trigger the first range finder
+;;    bsf	    flag_reg,	0   ; Set the flag_reg bit to show that the first range
+;;			    ; finder has been activated
+;    call    trigger_two
+;    goto    ranger_main
+;    return
+;    
+;ranger_init:
+;    clrf    time_one_H
+;    clrf    time_one_L
+;    clrf    time_two_H
+;    clrf    time_two_L
+;    bsf	    INTCON, 7	; Enable global interrupt
+;    bsf	    INTCON, 6   ; Enable peripheral interrupt
+;    bsf	    PIE4,   1	; Enable CCP4 interrupt
+;    bsf	    PIE4,   2	; Enable CCP5 interrupt
+;    bsf	    IPR4,   1	; Set CCP4 interrupt as high priority
+;    bsf	    IPR4,   2	; Set CCP5 interrupt as high priority
+;    clrf    flag_reg	; Clear flag register
+;    return 
+;        
+;interrupt_one:
+;    bcf	    PIE4,	1   ; Pause any other interrupts from CCP4
+;    bcf	    PIR4,	1   ; CCP4IF, clear the interrupt flag
+;    btfss   flag_reg,	1   ; Check if interrupt has already occurred
+;    goto    rising_one	    ; NO, start timer on the rising edge
+;    goto    falling_one	    ; YES, stop timer and move values into memory
+;        
+;interrupt_two:
+;    bcf	    PIE4,	2   ; Pause any other interrupts from CCP5
+;    bcf	    PIR4,	2   ; CCP5IF, clear the interrupt flag 	
+;    btfss   flag_reg,	2   ; Check if interrupt has already occurred
+;    goto    rising_two	    ; NO, start the timer on the rising edge
+;    goto    falling_two	    ; YES, stop timer and move values into memory
+;   
+;rising_one:
+;    clrf    CCP4CON
+;    movlw   0x04    
+;    movwf   CCP4CON	; Set the CCP4 control byte to capture mode and to 
+;			; trigger on the falling edge now
+;    clrf    CCPTMRS1 
+;    movlw   0x00	; CCP4 is based off of TMR1
+;    movwf   CCPTMRS1		
+;    bsf	    PIE4,   1	; Re-enable interrupts from CCP4
+;    movlw   0x31	; Set the TMR1 to use F_osc/4 prescaled by 8
+;    movwf   T1CON	; Start the timer
+;    return
+;    
+;falling_one:
+;    bcf	    PIE4,   1		; Disable CCP4 interrupts to prevent any further
+;				; interrupts
+;    bsf	    T1CON, 0		; Turn TMR1 off
+;    movff   TMR1L, time_one_L	; Move high byte into memory
+;    movff   TMR1H, time_one_H	; Move low byte into memory
+;    goto    ranger_main
+;    
+;    
+;rising_two:
+;    bcf	    PIE4,   1	; Disable CCP4 interrupts to prevent any further
+;			; interrupts
+;    clrf    CCP5CON
+;    movlw   0x04    
+;    movwf   CCP5CON	; Set the CCP5 control byte to capture mode and to 
+;			; trigger on the falling edge now
+;    clrf    CCPTMRS1 
+;    movlw   0x00	; CCP5 is based off of TMR1
+;    movwf   CCPTMRS1	
+;    movlw   0x31	; Set the TMR1 to use F_osc/4 prescaled by 8
+;    movwf   T1CON
+;    return
+;    
+;falling_two:
+;    bcf	    PIE4,   2		; Disable CCP4 interrupts to prevent any further
+;				; interrupts
+;    bsf	    T1CON,  0		; Turn TMR1 off
+;    movff   TMR1L, time_two_L	; Move high byte into memory
+;    movff   TMR1H, time_two_H	; Move low byte into memory
+;    return
+;    
+;trigger_one:
+;    ; Initialize the CCP interrupt and trigger the ultrasonic range finder.
+;    bcf	    PIR4,   1	; CCP4IF, clear the interrupt flag 
+;    clrf    CCP4CON
+;    movlw   0x00
+;    movwf   TRISG	; Make all pins on PORTG outputs.
+;    bsf	    LATG,   3	; Set pin 3 to be high.
+;    call    pulse_delay 
+;    bcf	    LATG,   3	; Set pin 3 to be low.
+;    movlw   0xFF
+;    movwf   TRISG	; Make all pins on PORTG inputs
+;    movlw   0x05	; Interrupt on rising edge.
+;    movwf   CCP4CON
+;    call    wait_delay
+;    return
+;
+;trigger_two:
+;    ; Initialize the CCP4 capture module interrupt and trigger the ultrasonic 
+;    ; range finder.
+;    bcf	    PIR4,   2	; CCP5IF, clear the interrupt flag 	
+;    clrf    CCP5CON
+;    movlw   0x00
+;    movwf   TRISG	; Make all pins on PORTH outputs.
+;    bsf	    LATG,   4	; Set pin 4 to be high.
+;    call    pulse_delay 
+;    bcf	    LATG,   4	; Set pin 4 to be low.
+;    movlw   0xFF
+;    movwf   TRISG	; Make all pins on PORTH inputs.
+;    movlw   0x05	; Interrupt on rising edge.
+;    movwf   CCP5CON
+;    call    wait_delay
+;    return
 
     
