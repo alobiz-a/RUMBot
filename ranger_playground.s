953d7cd37627b16b332aaaa97d92e5de5f2de03d
A Alonso Bizzi <ariannaalonsobizzi@gmail.com>
2023-12-14 22:16:05 +0000

    the INTERRRUPTS ARE ROLLONGGGGGG

--- ranger_playground.s
+++ ranger_playground.s
@@ -32,22 +32,35 @@ interrupt:
 
 ranger_main:   
     ;call    trigger_ranger
-    call    interrupt_setup ;Initialise the interrupts (ensure everything is cleared)
-    call    trigger_test
+    ;call    interrupt_setup ;Initialise the interrupts (ensure everything is cleared)
+    call    interrupt_clear
+    call    trigger_ranger
+    call    interrupt_setup
     call    wait_delay
     call    wait_delay
     call    wait_delay
     goto    ranger_main	;stay here until an interrupt causes us to jump out
-
+    
+interrupt_clear:
+    clrf	CCP4CON, A		    ;Disable CCP4
+    clrf	PIR4, A			    ;Clear all interrupt flags
+    bcf		PIE4, 1, A		    ;CCP4IE disabled
+    clrf	CCPR4L, A		    ;Clear CCP4 count
+    clrf	CCPR4H, A
+    movlw	01001000B		    ;Disable Timer 1
+    movwf	T1CON, A
+    return
+    
 interrupt_setup:
     ; Initialize interrupts and flags
     bsf	    INTCON, 6	; Enable all unmasked interrupts
     bsf	    INTCON, 7	; Enable peripheral interrupt
     bsf	    CCP4IE	; Enable interrupts
     ;A. QUESTION do we need to disable IPEN?
+    ;bcf	    IPEN
     bcf	    CCP4IF	; Clear interrupt flag (PIR4,0)
     bsf	    IPR4, 1	; Set CCP4 interrupt as high priority
-    bcf    ocurred,0	; Clear flag byte: an interrupt has not yet occured
+    clrf    ocurred	; Clear flag byte: an interrupt has not yet occured
     return
 
 trigger_test:
@@ -65,6 +78,10 @@ trigger_ranger:
     bsf     LATG, 3     ; Set pin 3 to be high
     call    pulse_delay ; 10 us delay
     bcf     LATG, 3	; Set pin 3 to be low.
+    nop
+    nop
+    nop
+    nop
     ;********Set pins as inputs*********
     movlw   0xFF	
     movwf   TRISG	; Make all pins on PORTG inputs
@@ -93,7 +110,7 @@ interrupt_rise:
     ;***********Clear interrupt flag and 
     bcf	    CCP4IE	;clear interrupt flag for these instructions
     bcf	    CCP4IF	;added
-    call    flash   ;for debugging
+    call    flash_rise   ;for debugging
     bsf   ocurred, 0 ; An interrupt has occured	
     ;**********TRIGGER ON FALLING EDGE************************
     clrf    CCP4CON
@@ -113,7 +130,7 @@ interrupt_rise:
 interrupt_fall:
     ;***********Clear interrupt flag and 
     bcf	    CCP4IE	;clear interrupt flag for these instructions
-    ;call    flash   ;for debugging
+    call    flash_fall   ;for debugging
     ;***********Turn off timer************
     bsf	    T1CON, 0		; Turn off TMR1
     movff   CCPR4H, time_one_H	; Move timer high byte into high byte storage
@@ -127,7 +144,7 @@ interrupt_fall:
  
     
        
-flash:
+flash_fall:
     clrf    LATA
     movlw   0x00
     movwf   TRISA	; Make all pins on PORTB outputs
@@ -140,6 +157,18 @@ flash:
     
     return
     
+flash_rise:
+    clrf    LATA
+    movlw   0x00
+    movwf   TRISA	; Make all pins on PORTB outputs
+    bsf     LATA, 3     ; Set pin 4 to be high
+    call    pulse_delay ; 10 us delay
+    call    pulse_delay ; 10 us delay
+    call    pulse_delay ; 10 us delay
+    bcf     LATA, 3	; Set pin 4 to be low.
+    clrf    LATA
+    
+    return
 
     end ranger_main
 
