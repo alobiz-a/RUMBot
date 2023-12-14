b871b7c6c6c688b67992a0ea1656d9801dde89da
A Alonso Bizzi <ariannaalonsobizzi@gmail.com>
2023-12-14 08:07:08 +0000

    testing Oli's code

--- main.s
+++ main.s
@@ -1,24 +1,68 @@
-	#include <xc.inc>
+#include <xc.inc>
 
 psect	code, abs
-	
-main:
-	org	0x0
-	goto	start
+extrn pulse_delay, ranging_delay, wait_delay
 
-	org	0x100		    ; Main code starts here at address 0x100
-start:
-	movlw 	0x0
-	movwf	TRISB, A	    ; Port C all outputs
-	bra 	test
-loop:
-	movff 	0x06, PORTB
-	incf 	0x06, W, A
-test:
-	movwf	0x06, A	    ; Test for end of loop condition
-	movlw 	0x63
-	cpfsgt 	0x06, A
-	bra 	loop		    ; Not yet finished goto start of loop again
-	goto 	0x0		    ; Re-run program from start
+rst:		; Reset vector
+    org 0x0000
+    goto start
+    
+int_hi: org 0x0008
+    goto bs_interrupt_routine
+    
+    
+start:		 ; Initialisation of ports and processes
+    bcf		CFGS	; point to Flash program memory  
+    bsf		EEPGD 	; access Flash program memory
+    clrf	LATE	; turn off PORTE
+    ;call	light_up
+    ;call	initialise_interrupts
+    call	light_up
+    call	trigger_ranger
+    call	pulse_delay
+    goto	start
 
-	end	main
+;***********STEPS*******************
+    ;0. Initialise the ranger
+    ;1. Trigger Ultrasound: Send trigger pulse to Ultrasound sensors
+    ;2. 
+;*************ROUTINES**************
+initialise_interrupts:
+    bsf	    INTCON, 6	; Enable peripheral interrupt
+    bsf	    INTCON, 7	; Enable global interrupt
+    bsf	    PIE4, 1	; Set CCP4 interrupt
+    bsf     IPR4, 1     ; Set CCP4 interrupt as high priority
+    clrf    PIR4	; Clear interrupt flag register
+    movlw   0x04
+    movwf   CCP4CON	; Set the CCP4 control byte to capture mode on the FALLING EDGE
+    return
+
+trigger_ranger:
+    movlw   0x00
+    movwf   TRISG	; Make all pins on PORTG outputs.
+    ;*****************************
+    bsf     LATG, 3	; Set pin 3 to be high.
+    call    pulse_delay
+    bcf	    LATG, 3
+    movlw   0xFF
+    movwf   TRISG	;set all pins to read mode
+    ;********************
+    return
+    
+bs_interrupt_routine:
+    ;**********LIGHT UP PORTE***********
+    movlw   0x00
+    movwf   TRISE
+    movlw   0xFF
+    movwf   LATE
+    clrf    PIR4
+
+    retfie  f
+
+light_up:
+    movlw   0x00
+    movwf   TRISE
+    movlw   0xFF
+    movwf   LATE
+    return
+    
---
+++ nbproject/delays.s
@@ -0,0 +1,59 @@
+#include <xc.inc>
+    
+psect delay_code, class = CODE
+ 
+global pulse_delay, ranging_delay, wait_delay
+
+pd1:	ds 1
+rd1:	ds 1
+rd2:	ds 1
+wd1:	ds 1
+wd2:	ds 1
+wd3:	ds 1
+
+;org 0x00
+pulse_delay: ; Generate 10 us delay.
+    movlw   0X22
+    movwf   pd1
+
+pulse_delay_loop:
+    nop
+    nop
+    decfsz  pd1, 1
+    goto    pulse_delay_loop
+    return
+    
+ranging_delay: ; Generate a 500 us delay.
+    movlw   0x61
+    movwf   rd1
+    movlw   0x0b
+    movwf   rd2
+
+ranging_delay_loop:
+    decfsz  rd1, 1
+    goto    ranging_delay_loop
+    decfsz  rd2, 1
+    goto    ranging_delay_loop
+    return    
+
+wait_delay: ; Generate a 60 ms delay. Made using an online delay generator.
+    movlw   0xBA
+    movwf   wd1
+    movlw   0xDF
+    movwf   wd2
+    movlw   0x05
+    movwf   wd3
+
+wait_delay_loop:
+    decfsz  wd1, 1
+    goto    wait_delay_loop
+    decfsz  wd2, 1
+    goto    wait_delay_loop
+    decfsz  wd3, 1
+    goto    wait_delay_loop
+    nop
+    return
+
+ end
+
+
