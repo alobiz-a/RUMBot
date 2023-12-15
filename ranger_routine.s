88526e11febb5db634035419fecdbfed645c1750
A Alonso Bizzi <ariannaalonsobizzi@gmail.com>
2023-12-15 01:53:44 +0000

    changes

--- ranger_routine.s
+++ ranger_routine.s
@@ -1,149 +1,172 @@
 #include <xc.inc>
-			  
-extrn pulse_delay, wait_delay
-        
-flag_reg set 0x20	    ; Define a flag to reflect if the first range finder
-			    ; has been activated or not.
-psect udata_acs
-time_one_H:	ds 1
-time_one_L:	ds 1
-time_two_H:	ds 1
-time_two_L:	ds 1
-			    
-global ranger_main, time_one_H, time_one_L, time_two_H, time_two_L 
-
-;psect ranger_code, class = CODE, abs
-psect ranger_code, class = CODE, abs
-org 0x00
-start:
-    goto    ranger_main
-
-
-interrupt_handler:
-    org 0x08
-    ; Interrupt vector
-    btfss   flag_reg, 0	    ; Check if the first range finder has been used
-    goto    interrupt_one   ; NO, measure the first range finder
-    goto    interrupt_two   ; YES, measure the second range finder
-    return  
-
-;org 0x100
-;org 0x2
-ranger_main:
-    org	    0x11A
-    call    ranger_init
-    call    trigger_one	    ; Trigger the first range finder
-    bsf	    flag_reg,	0   ; Set the flag_reg bit to show that the first range
-			    ; finder has been activated
-    call    trigger_two
-    goto    ranger_main
-    return
-    
-ranger_init:
-    clrf    time_one_H
-    clrf    time_one_L
-    clrf    time_two_H
-    clrf    time_two_L
-    bsf	    INTCON, 7	; Enable global interrupt
-    bsf	    INTCON, 6   ; Enable peripheral interrupt
-    bsf	    PIE4,   1	; Enable CCP4 interrupt
-    bsf	    PIE4,   2	; Enable CCP5 interrupt
-    bsf	    IPR4,   1	; Set CCP4 interrupt as high priority
-    bsf	    IPR4,   2	; Set CCP5 interrupt as high priority
-    clrf    flag_reg	; Clear flag register
-    return 
-        
-interrupt_one:
-    bcf	    PIE4,	1   ; Pause any other interrupts from CCP4
-    bcf	    PIR4,	1   ; CCP4IF, clear the interrupt flag
-    btfss   flag_reg,	1   ; Check if interrupt has already occurred
-    goto    rising_one	    ; NO, start timer on the rising edge
-    goto    falling_one	    ; YES, stop timer and move values into memory
-        
-interrupt_two:
-    bcf	    PIE4,	2   ; Pause any other interrupts from CCP5
-    bcf	    PIR4,	2   ; CCP5IF, clear the interrupt flag 	
-    btfss   flag_reg,	2   ; Check if interrupt has already occurred
-    goto    rising_two	    ; NO, start the timer on the rising edge
-    goto    falling_two	    ; YES, stop timer and move values into memory
    
-rising_one:
-    clrf    CCP4CON
-    movlw   0x04    
-    movwf   CCP4CON	; Set the CCP4 control byte to capture mode and to 
-			; trigger on the falling edge now
-    clrf    CCPTMRS1 
-    movlw   0x00	; CCP4 is based off of TMR1
-    movwf   CCPTMRS1		
-    bsf	    PIE4,   1	; Re-enable interrupts from CCP4
-    movlw   0x31	; Set the TMR1 to use F_osc/4 prescaled by 8
-    movwf   T1CON	; Start the timer
+extrn pulse_delay, wait_delay
+global ranger_main, time_one_L, time_one_H
+   
+psect udata_acs
+ 
+ocurred:    ds 1 ; Has an interrupt happened (c.a.d. we on the rising or falling edge)?
+time_one_L: ds 1    ;low bit for ranger 1
+time_one_H: ds 1    ;high bit for ranger 1
+
+   
+psect code, class = CODE, abs
+ 
+rst:
+    org 0x00??????; Reset vector
+    clrf    PORTG
+    goto ranger_main
+
+   
+interrupt:
+    org?????    0x08
+    ;bcf    CCP4IF
+    btfss   CCP4IF  
+    retfie  f;is this necessary?
+    bcf?????    CCP4IF
+    btfss   ocurred, 0, A???????????; Has an interrupt already happened?
+    goto    interrupt_rise????; No, go to rising edge interrupt
+    goto    interrupt_fall????; Yes, go to falling edge interrupt
+    ;might need to check if correct CCP module
+
+ranger_main:  
+    ;call    trigger_ranger
+    ;call    interrupt_setup ;Initialise the interrupts (ensure everything is cleared)
+    call    interrupt_clear
+    call    trigger_ranger
+    call    interrupt_setup
+    call    wait_delay
+    call    wait_delay
+    call    wait_delay
+    goto    ranger_main?;stay here until an interrupt causes us to jump out
+   
+interrupt_clear:
+    clrf????CCP4CON, A????????    ;Disable CCP4
+    clrf????PIR4, A?????????????????    ;Clear all interrupt flags
+    bcf???????????PIE4, 1, A????????    ;CCP4IE disabled
+    clrf????CCPR4L, A?????????    ;Clear CCP4 count
+    clrf????CCPR4H, A
+    movlw???01001000B?????????    ;Disable Timer 1
+    movwf???T1CON, A
     return
-    
-falling_one:
-    bcf	    PIE4,   1		; Disable CCP4 interrupts to prevent any further
-				; interrupts
-    bsf	    T1CON, 0		; Turn TMR1 off
-    movff   TMR1L, time_one_L	; Move high byte into memory
-    movff   TMR1H, time_one_H	; Move low byte into memory
-    goto    ranger_main
-    
-    
-rising_two:
-    bcf	    PIE4,   1	; Disable CCP4 interrupts to prevent any further
-			; interrupts
-    clrf    CCP5CON
-    movlw   0x04    
-    movwf   CCP5CON	; Set the CCP5 control byte to capture mode and to 
-			; trigger on the falling edge now
-    clrf    CCPTMRS1 
-    movlw   0x00	; CCP5 is based off of TMR1
-    movwf   CCPTMRS1	
-    movlw   0x31	; Set the TMR1 to use F_osc/4 prescaled by 8
-    movwf   T1CON
+   
+interrupt_setup:
+    ; Initialize interrupts and flags
+    bsf?????    INTCON, 6?????; Enable all unmasked interrupts
+    bsf?????    INTCON, 7?????; Enable peripheral interrupt
+    bsf?????    CCP4IE??; Enable interrupts
+    ;A. QUESTION do we need to disable IPEN?
+    ;bcf????    IPEN
+    bcf?????    CCP4IF??; Clear interrupt flag (PIR4,0)
+    bsf?????    IPR4, 1?; Set CCP4 interrupt as high priority
+    clrf    ocurred?????; Clear flag byte: an interrupt has not yet occured
     return
-    
-falling_two:
-    bcf	    PIE4,   2		; Disable CCP4 interrupts to prevent any further
-				; interrupts
-    bsf	    T1CON,  0		; Turn TMR1 off
-    movff   TMR1L, time_two_L	; Move high byte into memory
-    movff   TMR1H, time_two_H	; Move low byte into memory
-    return
-    
-trigger_one:
-    ; Initialize the CCP interrupt and trigger the ultrasonic range finder.
-    bcf	    PIR4,   1	; CCP4IF, clear the interrupt flag 
-    clrf    CCP4CON
+
+trigger_test:
+    bsf     LATG, 3     ; Set pin 3 to be high
+    call    pulse_delay ; 10 us delay
+    bcf     LATG, 3?????; Set pin 3 to be low.
+   
+trigger_ranger:
+    ;********Disable interrupts************
+    bcf?????    CCP4IE??; Disable interrupts
+    ;********Setting the pins to outputs*******
     movlw   0x00
-    movwf   TRISG	; Make all pins on PORTG outputs.
-    bsf	    LATG,   3	; Set pin 3 to be high.
-    call    pulse_delay 
-    bcf	    LATG,   3	; Set pin 3 to be low.
-    movlw   0xFF
-    movwf   TRISG	; Make all pins on PORTG inputs
-    movlw   0x05	; Interrupt on rising edge.
+    movwf   TRISG?; Make all pins on PORTG outputs
+    ;********Send pulse*************
+    bsf     LATG, 3     ; Set pin 3 to be high
+    call    pulse_delay ; 10 us delay
+    bcf     LATG, 3?????; Set pin 3 to be low.
+    nop
+    nop
+    nop
+    nop
+    ;********Set pins as inputs*********
+    movlw   0xFF??
+    movwf   TRISG?; Make all pins on PORTG inputs
+    ;********Set up to capture on rising edge******
+    clrf    CCP4CON?????
+    movlw   0x05????????
+    movwf   CCP4CON?????; Set the CCP4 control byte to capture mode and to
+    ??????????????; trigger on the rising edge (CCP4M = 0101)
+    ;********Set PORTG to digital**************( A. PUT IN SETUP!!)*******
+    banksel ANCON2??????;points to it
+    clrf    ANCON2??????; 0 = digital
+    banksel 0???????????; default
+    ;***********SET PORTE AS OUTPUT FOR DEBUGGING PURPOSES************
+    clrf    TRISE?
+    movff   PORTG, LATE?; Display what's happening on G to E to prove we can read
+;    goto    tstlp   ;INFINITE?????
+;    call    wait_delay
+    ;*************************(ABOVE GOES IN SETUP)***********************
+    ;*************Re-enable interrupts***********************
+    bsf?????    CCP4IE??; Enable interrupts
+    return
+
+   
+interrupt_rise:
+
+    ;***********Clear interrupt flag and
+    bcf?????    CCP4IE??;clear interrupt flag for these instructions
+    bcf?????    CCP4IF??;added
+    call    flash_rise   ;for debugging
+    bsf   ocurred, 0 ; An interrupt has occured?
+    ;**********TRIGGER ON FALLING EDGE************************
+    clrf    CCP4CON
+    movlw   0x04    
     movwf   CCP4CON
-    call    wait_delay
-    return
-
-trigger_two:
-    ; Initialize the CCP4 capture module interrupt and trigger the ultrasonic 
-    ; range finder.
-    bcf	    PIR4,   2	; CCP5IF, clear the interrupt flag 	
-    clrf    CCP5CON
+    ;*********START TIMERS**********
     movlw   0x00
-    movwf   TRISG	; Make all pins on PORTH outputs.
-    bsf	    LATG,   4	; Set pin 4 to be high.
-    call    pulse_delay 
-    bcf	    LATG,   4	; Set pin 4 to be low.
-    movlw   0xFF
-    movwf   TRISG	; Make all pins on PORTH inputs.
-    movlw   0x05	; Interrupt on rising edge.
-    movwf   CCP5CON
-    call    wait_delay
+    movwf   CCPTMRS1????; CCP4 is based off of TMR1
+    clrf    TMR1H   ;clear timers just in case
+    clrf    TMR1L
+    movlw   0x33    ; that's turning on the timer?????- CHANGED TO 0X33 FROM 0X31
+    movwf   T1CON?; Set TMR1 to use F_osc/4 prescaled by a factor of 8, INITIALISE TIMER
+    bsf?????    CCP4IE ?; Re-enable the CCP4 interrupt (INTERRUPT ENABLE bit) - A. CHECK W MARK!
+    bcf?????    CCP4IF
+    retfie??f
+   
+interrupt_fall:
+    ;***********Clear interrupt flag and
+    bcf?????    CCP4IE??;clear interrupt flag for these instructions
+    call    flash_fall   ;for debugging
+    ;***********Turn off timer************
+    bsf?????    T1CON, 0????????????; Turn off TMR1
+    movff   CCPR4H, time_one_H??????; Move timer high byte into high byte storage
+    movff   CCPR4L, time_one_L??????; Move timer low byte into low byte storage
+    bcf?????    CCP4IF  ; need to add!
+    bcf    ocurred,0????; Clear flag byte: an interrupt has not yet occured
+   
+    retfie  f    
+
+
+ 
+   
+       
+flash_fall:
+    clrf    LATA
+    movlw   0x00
+    movwf   TRISA?; Make all pins on PORTB outputs
+    bsf     LATA, 4     ; Set pin 4 to be high
+    call    pulse_delay ; 10 us delay
+    call    pulse_delay ; 10 us delay
+    call    pulse_delay ; 10 us delay
+    bcf     LATA, 4?????; Set pin 4 to be low.
+    clrf    LATA
+   
+    return
+   
+flash_rise:
+    clrf    LATA
+    movlw   0x00
+    movwf   TRISA?; Make all pins on PORTB outputs
+    bsf     LATA, 3     ; Set pin 4 to be high
+    call    pulse_delay ; 10 us delay
+    call    pulse_delay ; 10 us delay
+    call    pulse_delay ; 10 us delay
+    bcf     LATA, 3?????; Set pin 4 to be low.
+    clrf    LATA
+   
     return
 
-    
-
-
+    end ranger_main
