ce2515d85c7949c3526e81c88299c84fe036338b
A Alonso Bizzi <ariannaalonsobizzi@gmail.com>
2023-12-15 10:45:28 +0000

    UART, LCD and conversion work

--- LCDtest_main.s
+++ LCDtest_main.s
@@ -2,14 +2,16 @@
 #include <xc.inc>
     
 ;SHOULD OUTPUT DIST_L AND DIST_H
+extrn  format_for_display
+global time_H, time_L
 extrn	display_on_LCD	; in dist_on_LCD.s
 extrn	LCD_Setup   ; in LCD_routines.s
-global	dist_H, dist_L
+extrn	UART_Setup, send_dists_UART  ; in UART_routines.s
     
     
 psect	udata_acs   ; reserve data space in access ram
-dist_H:	    ds 1    ; reserve 1 byte to store the distance measured at each step
-dist_L:	    ds 1    ; store nibble to transmit to display
+time_H:	    ds 1    ; reserve 1 byte to store the distance measured at each step
+time_L:	    ds 1    ; store nibble to transmit to display
 ;no variables needed   
 psect	code, abs ; absolute address
 	
@@ -24,19 +26,21 @@ rst:
 setup:
     org	0x100
     	; ******* Programme FLASH read Setup Code ***********************
-    bcf	CFGS	; point to Flash program memory  
-    bsf	EEPGD 	; access Flash program memory
+    bcf	    CFGS	; point to Flash program memory  
+    bsf	    EEPGD 	; access Flash program memory
     call    LCD_Setup
+    call    UART_Setup
     goto	main
 
 
 main:
-    movlw   0x34
-    movwf   dist_H
-    movlw   0x93
-    movwf   dist_L
-    ;call    format_for_display
+    movlw   0x11
+    movwf   time_H
+    movlw   0x02
+    movwf   time_L
+    call    format_for_display
     call    display_on_LCD
+    call    send_dists_UART
     goto $
 
 
---
+++ UART_routines.s
@@ -0,0 +1,67 @@
+#include <xc.inc>
+    
+global  UART_Setup, send_dists_UART
+extrn	dist_H, dist_L
+
+
+psect	udata_acs   ; reserve data space in access ram
+UART_counter: ds    1	    ; reserve 1 byte for variable UART_counter
+UART_hex_tmp:	ds 1    ; reserve 1 byte for variable UART_hex_tmp - A. CHANGE
+UART_tmp:	ds 1	; reserve 1 byte for temporary use - A. CHANGE
+
+psect	uart_code,class=CODE
+UART_Setup:
+    bsf	    SPEN	; enable
+    bcf	    SYNC	; synchronous
+    bcf	    BRGH	; slow speed
+    bsf	    TXEN	; enable transmit
+    bcf	    BRG16	; 8-bit generator only
+    movlw   103		; gives 9600 Baud rate (actually 9615)
+    movwf   SPBRG1, A	; set baud rate
+    bsf	    TRISC, PORTC_TX1_POSN, A	; TX1 pin is output on RC6 pin
+					; must set TRISC6 to 1
+    return
+
+UART_Transmit_Message:	    ; Message stored at FSR2, length stored in W
+    movwf   UART_counter, A
+UART_Loop_message:
+    movf    POSTINC2, W, A
+    call    UART_Transmit_Byte
+    decfsz  UART_counter, A
+    bra	    UART_Loop_message
+    return
+
+UART_Transmit_Byte:	    ; Transmits byte stored in W
+    btfss   TX1IF	    ; TX1IF is set when TXREG1 is empty
+    bra	    UART_Transmit_Byte
+    movwf   TXREG1, A
+    return
+;******************DO I NEED THIS (RENAME)?***************************
+; Same logic as for LCD
+UART_Transmit_Hex:			; Writes byte stored in W as hex
+	movwf	UART_hex_tmp, A
+	swapf	UART_hex_tmp, W, A	; high nibble first
+	call	UART_Hex_Nib
+	movf	UART_hex_tmp, W, A	; then low nibble
+UART_Hex_Nib:			; writes low nibble as hex character
+	andlw	0x0F
+	movwf	UART_tmp, A
+	movlw	0x0A
+	cpfslt	UART_tmp, A
+	addlw	0x07		; number is greater than 9 
+	addlw	0x26
+	addwf	UART_tmp, W, A
+	call	UART_Transmit_Byte ; write out ascii
+	return
+	
+send_dists_UART: ;hex to ASCII
+	movf	dist_H,0
+	call	UART_Transmit_Hex
+	movf	dist_L,0
+	call	UART_Transmit_Hex
+	movlw	0x0A
+	call	UART_Transmit_Byte
+	return
+	
+
+
